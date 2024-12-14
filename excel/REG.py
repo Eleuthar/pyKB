e74b5727e2d08a33aa611aa5ca71b82c3d0c2f3a
Eleuthar <flbujoreanu@gmail.com>
2024-12-14 19:58:32 +0200

    Update REG.py

--- excel/REG.py
+++ excel/REG.py
@@ -1,6 +1,8 @@
+from urllib import request
 import openpyxl
 from openpyxl.styles import Alignment
 import argparse
+from pdb import set_trace
 
 
 class CustomArgParser(argparse.ArgumentParser):
@@ -10,36 +12,120 @@ class CustomArgParser(argparse.ArgumentParser):
         'Executati scriptul astfel: python REG.py <numele fisierului excel>')
         exit(2)
 
+
 def menu_input(frame, prod, opt):
     print('Pret unitar curent\n')
-    for x in range(1, 8):
-        pret = frame['E'][x+9]
-        prod[x]['$'] = pret
-        print(f"{x}. {prod[x]['prod']} = {pret:.2f}")
+    
     while True:
+        for x in range(1, 8):
+            set_trace() # <<<<<<<<<<<<<<<<<<<<<<<<<<
+            pret = float(frame['E'][x+9].value)
+            prod[x]['$'] = pret
+            print(f"{x}. {prod[x]['prod']} = {pret:.2f}")
         menu = input('Introduceti numar articol de modificat: ')
         try:
             menu = int(menu)
             if menu in opt:
                 return menu
-            except:
-                print(f'Optiunea introdusa >> {menu} << este invalida')
-                continue
+        except:
+            print(f'Optiunea introdusa >> {menu} << este invalida\n')
+
+
+def prompt_unit_price(x, frame, prod):
+    opt = [x for x in range(1,8)]
+    while True:
+        unit = input('Modificati pret unitar?\nApasati "Enter" pentru "DA"\nApasati "Esc" urmat de "Enter" pt "NU" ')
+        if unit == '':
+            menu = menu_input(frame, prod, opt)
+            if menu in opt:
+                while True:
+                    pret = input('Introduceti pret unitar nou cu zecimale separate de ",": ')
+                try:
+                    pret = pret.replace(',','.')
+                    frame['E'][9+menu].value = float(pret)
+                    break
+                except:
+                    pret = input(f'Valoare invalida: {pret}.\n')
+        else:
+            break
+
+
+# metoda temporara pt TRANSFER format fizic
+def paper_format_transfer(x, prod, frame, prev):
+    added = 0
+    reg_quant = 'None'
+    prod_name = prod[x-9]['prod']
+    prod_quant =  prod[x-9]['$']
+    prev_quant = prev[f'I{x}'].value
+
+    if prev_quant is None:
+        prev_quant = 0
+    else:
+        prev_quant = int(prev_quant)
+    
+    while reg_quant != '' or reg_quant.isnumeric(): 
+        reg_quant = input(f'\nCantitate {prod_name} registru vechi (stoc anterior = {prev_quant}) >> ')
+    
+    if reg_quant == '':
+        reg_quant = 0
+    else:
+        reg_quant = int(reg_quant)
+        added = reg_quant-prev_quant
+
+    frame[f'B{x}'] = added
+    frame[f'C{x}'] = prev_quant
+    print(f"Stoc anterior = {prev_quant}, Adaugari = {added}")
+
 
 def in_out(mode, row):
     # mode == 'B'||'G'
     # row == 10:16 
     param = {'B': 'Intrari', 'G': 'Iesiri'}
-    nir = input(param[mode] + " " + frame[f'A{row}'].value + ": ")
-    if nir is None or nir == '':
-        nir = 0
-    frame[f'{mode}{row}'].value = nir
+    nir = None
+    while not nir.isnumeric():
+        nir = input(param[mode] + " " + frame[f'A{row}'].value + f": ")
+        if nir is None or nir == '':
+            nir = '0'
+    frame[f'{mode}{row}'].value = int(nir)
+
+    
+def initialize(frame):
+    for x in range(10,17):
+        for q in ('B', 'C', 'E', 'F', 'G', 'H', 'I', 'J'):
+            tgt = frame[f'{q}{x}']
+            # Reset to 0
+            if q in ('B','C','F','G','H','J'):
+                tgt.value = 0
+            # unit price
+            if q == 'E':
+                tgt.value = str(tgt.value)
+                if tgt.value[-3] == ',':
+                    tgt.value = float(tgt.value.replace(",", "."))
+            # move I quantity to C & reset I
+            if q == 'I':
+                if tgt.value in (None, ''):
+                    tgt.value = 0
+                frame[f'C{x}'].value = tgt.value
+                tgt.value = 0
+                
+
+ # clear all 0 cells before saving the worksheet
+def zero_to_none_or_float(frame):
+    for x in range(10,17):
+        for q in ('B', 'C', 'E', 'F', 'G', 'H', 'I', 'J'):
+            tgt = frame[f'{q}{x}']
+            if tgt.value in (0, '0'):
+                tgt.value = None
+            elif q in ('E','F','H','J') and tgt.value is not None:
+                tgt.value = ('%.2f' % tgt.value).replace('.', ',')
+
 
 parser = CustomArgParser(description="Example script with custom error handling")
 parser.add_argument("file", type=str)
 argz = parser.parse_args()
 xxpath = argz.file
-workbook = openpyxl.load_workbook(xxpath)
+workbook = openpyxl.load_workbook(xxpath, data_only=True)
+
 prod = {
     1: {'prod':'Lumanari 100B', '$': 0},
     2: {'prod':'Lumanari C20', '$': 0},
@@ -49,6 +135,7 @@ prod = {
     6: {'prod':'Candele tip 3', '$': 0},
     7: {'prod':'Candele, tip 4', '$': 0}
 }
+
 while True:
     prev = workbook.worksheets[-1]
     frame = workbook.copy_worksheet(prev)
@@ -57,72 +144,38 @@ while True:
     total_j = frame['J18']
     total_j.value = 0
     enter_year = input('An registru: ')
-    enter_dm = input('Zi + luna registru (ex. 12 Dec): ')
+    enter_dm = input('Zi + luna registru (ex. 12 DEC): ').upper()
     frame['F2'].value = f'DATA: {enter_dm} {enter_year}'
-    frame.title = f"{enter_dm} {enter_year}"
-    # convert None or '' to 0
-    for j in ['B', 'C', 'E', 'F', 'G', 'H', 'I', 'J']:
-        for row in range(10, 17):
-            tgt = frame[f'{j}{row}']
-            if tgt.value == '' or tgt.value is None:
-                tgt.value = 0
-            elif j == 'E' and isinstance(tgt.value, str):
-                tgt.value = float(tgt.value.replace(",", "."))
-            # Importare sheet anterior I10:16
-            if j == 'I':
-                frame[f'C{row}'].value = tgt.value
-                tgt.value = 0
-            # Resetare valori iesire
-            if j == 'J':
-                tgt.value = 0
-    # update pret unitar
-    opt = [x for x in range(1, 8)]
-    while True:
-        unit = input('Modificati pret unitar?\nApasati "Enter" pentru "DA"\nApasati "Esc" urmat de "Enter" pt "NU" ')
-        if unit == '':
-            menu = menu_input(frame, prod, opt)
-            if menu in opt:
-                while True
-                    pret = input('Introduceti pret unitar nou cu zecimale separate de ",": ')
-                    try
-                        pret = pret.replace(',','.')
-                        frame['E'][x+menu] = pret
-                        break
-                    except
-                        pret = input(f'Valoare invalida: {pret}.\n')
-        else:
-            break            
-    # parser
-    for x in range(10, 17):
-        # B & G 10:16 intrari\iesiri
+    frame.title = enter_dm
+    initialize(frame)
+    prompt_unit_price(frame, prod)
 
-        """TRANSFER format fizic ONLY"""
-        new_reg_quant = int(input('Cantitate registru nou > '))
-        added = new_reg_quant - int(prev['I'][x].value)
-        prev_quant = new_reg_quant - added
-        frame['B'][x] = added
-        frame['C'][x] = prev_quant
-        print(f"Stoc anterior = {prev_quant}, Adaugari = {added}")
+    # B & G 10:16 in\out parser
+    for x in range(10, 17):
+        # comment below after migration from paper
+        paper_format_transfer(x, prod, frame, prev)
         
-        """De decomentat dupa transfer registru fizic in care lipseste Adaugari"""
+        # regular flow
+        # uncomment below after migration from paper where "Adaugari" is missing
         # in_out('B', x)
         in_out('G', x)
 
         # F \\ Valoare totala intrari
-        frame[f'F{x}'].value = (int(frame[f'B{x}'].value) + int(frame[f'C{x}'].value)) * int(frame[f'E{x}'].value)
+        frame[f'F{x}'].value = (float(frame[f'B{x}'].value) + float(frame[f'C{x}'].value)) * float(frame[f'E{x}'].value)
         total_f.value += frame[f'F{x}'].value
         
         # H \\ Valoare totala iesiri
-        frame[f'H{x}'].value = int(frame[f'E{x}'].value) * int(frame[f'G{x}'].value)
+        frame[f'H{x}'].value = float(frame[f'E{x}'].value) * float(frame[f'G{x}'].value)
         
         # I \\ Cantitate stoc ramas
         frame[f'I{x}'].value = (int(frame[f'B{x}'].value) + int(frame[f'C{x}'].value)) - int(frame[f'G{x}'].value)
         
         # J \\ Valoare stoc ramas
-        frame[f'J{x}'].value = int(frame[f'E{x}'].value) * int(frame[f'I{x}'].value)
+        frame[f'J{x}'].value = float(frame[f'E{x}'].value) * float(frame[f'I{x}'].value)
         total_j.value += frame[f'J{x}'].value
-
+    
+    zero_to_none_or_float(frame)
     workbook.save(xxpath)
     nxt = input('Adaugati registru nou?\nApasati "Enter" pentru a continua\nApasati "Esc" urmat de "Enter" pt a iesi')
-    if nxt != (''):
+    if nxt != '':
         break
